04319d1bb9915811ab192cca7fcc5c0588ed06c3
Vale-n-tina <valentia.grg@gmail.com>
2025-03-01 21:54:23 +0100

    .

---
+++ src/types/PriceData.ts
@@ -0,0 +1,9 @@
+interface PriceData {
+    distanceM: number | null;
+    passengers: number;
+    suitcases: number;
+    backpack: number;
+    pickUpTime: string; 
+    childSeats: string;
+  }
+  export default PriceData;
