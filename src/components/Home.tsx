dfd4ff1ed86e18385ac9ccb0499b161d7630e377
Vale-n-tina <valentia.grg@gmail.com>
2025-03-05 12:29:59 +0100

    .

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,26 +1,41 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Home = () => {
   return (
     <div className="bg-imageSfondo">
-      <Container>
+      <Container style={{ position: "relative", zIndex: 2 }}>
         <Row className="justify-content-center align-items-center">
-            <Col className="text-center col-12 text-white mt-5">
+          <Col className="text-center col-12 text-white mt-5">
             <h1 className="title-shadow mb-4 mt-5">Rome Chauffeured Car</h1>
-            </Col>
-            <Col className="col-10">
+          </Col>
+          <Col className="col-10">
             <p className="text-white text-center p-fade-in code mt-3 mb-5 ">
-            An exclusive service in Rome offering private transfers, personalized tours, and trips to any location. Enjoy comfort, reliability, and professionalism at every step.
+              An exclusive <span className="fw-bold">service in Rome</span>{" "}
+              offering private transfers, personalized tours, and trips to any
+              location. Enjoy comfort, reliability, and professionalism at every
+              step.
             </p>
-            </Col>
-            <Col className="col-10">
-            <Row>
-                <Col className="col-5"> button</Col>
-                <Col className="col-5">button</Col>
+          </Col>
+          <Col className="col-12">
+            <Row className=" justify-content-center">
+              <Col className="col-8 mb-3">
+                <Button variant="gold" className="custom-gold-btn fw-bold sans">
+                  BOOK NOW
+                </Button>
+              </Col>
+              <Col className="col-8 mb-5">
+                <Button
+                  variant="dark"
+                  className="custom-black-btn fw-bold sans"
+                >
+                  VIEW TOUR
+                </Button>
+              </Col  >
+              <Col  className="col-8 mb-5">
+              </Col>
             </Row>
-            </Col>
+          </Col>
         </Row>
-        
       </Container>
     </div>
   );
