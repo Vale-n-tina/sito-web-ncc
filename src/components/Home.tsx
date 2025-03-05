0dbad98e8869e68d4242ba829f6b9e6a81944017
Vale-n-tina <valentia.grg@gmail.com>
2025-03-05 14:06:06 +0100

    .

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,12 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div className="bg-imageSfondo">
-      <Container style={{ position: "relative", zIndex: 2 }}>
-        <Row className="justify-content-center align-items-center">
+    <Container fluid className="p-0">
+      <div className="bg-imageSfondo pb-5">
+        <Row
+          className="justify-content-center align-items-center"
+          style={{ position: "relative", zIndex: 2 }}
+        >
           <Col className="text-center col-12 text-white mt-5">
             <h1 className="title-shadow mb-4 mt-5">Rome Chauffeured Car</h1>
           </Col>
@@ -30,14 +33,51 @@ const Home = () => {
                 >
                   VIEW TOUR
                 </Button>
-              </Col  >
-              <Col  className="col-8 mb-5">
               </Col>
             </Row>
           </Col>
         </Row>
-      </Container>
-    </div>
+      </div>
+      <Row className="no-margin-padding justify-content-center">
+        <Col className="text-center col-12 p-0 no-margin-padding">
+          <h1 className="h1-presentation ">Why choose us?</h1>
+        </Col>
+        <Col className="col-8 text-center mt-4">
+          <i className="bi bi-clock "></i>
+          <h2 className="mt-2 mb-3  sans fw-bold">Punctuality</h2>
+          <p className="quicksand">
+            Always available to assist you, offering quick and reliable
+            transfers for a smooth and worry-free journey
+          </p>
+          <p className="quicksand fst-italic text-decoration-underline">
+            No waiting, just smooth and reliable transfers.
+          </p>
+        </Col>
+        <Col className="col-8 text-center mt-4 ">
+          <i className="bi bi-gem"></i>
+          <h2 className="mt-2 mb-3  sans fw-bold">Comfort</h2>
+          <p className="quicksand">
+            Vehicles equipped with all the comforts for a stress-free and
+            comfortable journey.
+          </p>
+          <p className="quicksand fst-italic text-decoration-underline">
+            Incredible experiences tailored for your ultimate comfort.
+          </p>
+        </Col>
+
+        <Col className="col-8 text-center mt-4 ">
+          <i className="bi bi-award-fill"></i>
+          <h2 className="mt-2 mb-3  sans fw-bold">Personalization</h2>
+          <p className="quicksand">
+            Transfers and tours tailored to meet your individual preferences and
+            requirements, ensuring a personalized experience every time.
+          </p>
+          <p className="quicksand fst-italic text-decoration-underline">
+          Each aspect is crafted with your needs in mind
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Home;
