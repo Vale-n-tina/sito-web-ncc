a6770989562628618f2a6e093c031ceaf6f2d434
Vale-n-tina <valentia.grg@gmail.com>
2025-03-05 17:44:45 +0100

    .

--- src/components/AccordionFAQ.tsx
+++ src/components/AccordionFAQ.tsx
@@ -2,10 +2,10 @@ import Accordion from "react-bootstrap/Accordion";
 
 function AccordionFAQ() {
   return (
-    <Accordion defaultActiveKey="0" className="custom-accordion mb-5">
-    <Accordion.Item eventKey="0">
+    <Accordion defaultActiveKey="0" className="custom-accordion mb-5 mt-3">
+    <Accordion.Item eventKey="0" className="mb-3">
       <Accordion.Header>
-        <i className="bi bi-plus-square-fill"></i>Accordion Item #1
+    Accordion Item #1
       </Accordion.Header>
       <Accordion.Body className="text-black">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -17,9 +17,21 @@ function AccordionFAQ() {
         culpa qui officia deserunt mollit anim id est laborum.
       </Accordion.Body>
     </Accordion.Item>
-    <Accordion.Item eventKey="1">
+    <Accordion.Item eventKey="1" className="mb-3">
       <Accordion.Header>Accordion Item #2</Accordion.Header>
-      <Accordion.Body>
+      <Accordion.Body  className="text-black">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </Accordion.Body>
+    </Accordion.Item>
+    <Accordion.Item eventKey="2" className="mb-3">
+      <Accordion.Header>Accordion Item #2</Accordion.Header>
+      <Accordion.Body  className="text-black">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
         minim veniam, quis nostrud exercitation ullamco laboris nisi ut
