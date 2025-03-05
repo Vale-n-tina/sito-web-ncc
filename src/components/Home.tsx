286658bc87b3daeda0aca75c58fb102f82388015
Vale-n-tina <valentia.grg@gmail.com>
2025-03-05 17:13:50 +0100

    .

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
+import AccordionFAQ from "./AccordionFAQ";
 
 const Home = () => {
   return (
@@ -8,10 +9,10 @@ const Home = () => {
           className="justify-content-center align-items-center"
           style={{ position: "relative", zIndex: 2 }}
         >
-          <Col className="text-center col-12 text-white mt-5">
+          <Col className="text-center col-12 col-md-6 text-white mt-5">
             <h1 className="title-shadow mb-4 mt-5">Rome Chauffeured Car</h1>
           </Col>
-          <Col className="col-10">
+          <Col className="col-10 col-md-7">
             <p className="text-white text-center p-fade-in code mt-3 mb-5 ">
               An exclusive <span className="fw-bold">service in Rome</span>{" "}
               offering private transfers, personalized tours, and trips to any
@@ -21,15 +22,18 @@ const Home = () => {
           </Col>
           <Col className="col-12">
             <Row className=" justify-content-center">
-              <Col className="col-8 mb-3">
-                <Button variant="gold" className="custom-gold-btn fw-bold sans">
+              <Col className="col-8 col-md-3 col-lg-2 mb-3">
+                <Button
+                  variant="gold"
+                  className="custom-gold-btn fw-bold sans p-md-2"
+                >
                   BOOK NOW
                 </Button>
               </Col>
-              <Col className="col-8 mb-5">
+              <Col className="col-8 col-md-3  col-lg-2 mb-5">
                 <Button
                   variant="dark"
-                  className="custom-black-btn fw-bold sans"
+                  className="custom-black-btn fw-bold sans p-md-2"
                 >
                   VIEW TOUR
                 </Button>
@@ -39,10 +43,12 @@ const Home = () => {
         </Row>
       </div>
       <Row className="no-margin-padding justify-content-center">
-        <Col className="text-center col-12 p-0 no-margin-padding">
-          <h1 className="h1-presentation ">Why choose us?</h1>
+        <Col className="text-center col-8 col-md-9 p-0 no-margin-padding mt-2 mb-md-3 ">
+          <h1 className="h1-presentation mb-4 mb-md-5 ">Why choose us?</h1>
+          <hr className=" " />
         </Col>
-        <Col className="col-8 text-center mt-4">
+
+        <Col className="col-8 col-md-4 text-center mt-3">
           <i className="bi bi-clock "></i>
           <h2 className="mt-2 mb-3  sans fw-bold">Punctuality</h2>
           <p className="quicksand">
@@ -53,7 +59,7 @@ const Home = () => {
             No waiting, just smooth and reliable transfers.
           </p>
         </Col>
-        <Col className="col-8 text-center mt-4 ">
+        <Col className="col-8 col-md-4 text-center mt-4 ">
           <i className="bi bi-gem"></i>
           <h2 className="mt-2 mb-3  sans fw-bold">Comfort</h2>
           <p className="quicksand">
@@ -65,7 +71,7 @@ const Home = () => {
           </p>
         </Col>
 
-        <Col className="col-8 text-center mt-4 ">
+        <Col className="col-8 col-md-4 text-center mt-4  mb-3">
           <i className="bi bi-award-fill"></i>
           <h2 className="mt-2 mb-3  sans fw-bold">Personalization</h2>
           <p className="quicksand">
@@ -73,8 +79,50 @@ const Home = () => {
             requirements, ensuring a personalized experience every time.
           </p>
           <p className="quicksand fst-italic text-decoration-underline">
-          Each aspect is crafted with your needs in mind
+            Each aspect is crafted with your needs in mind
           </p>
+        </Col>
+      </Row>
+      <div className="bg-imageSfondo2 pb-5">
+        <Row
+          style={{ position: "relative", zIndex: 2 }}
+          className=" justify-content-center"
+        >
+          <Col className="text-center mt-5 col-9">
+            <h1 className="text-white merriweather mb-4 ">
+              Contact Us Anytime
+            </h1>
+            <p className="text-white mb-4">
+              We’re always here to answer your questions and provide the best
+              assistance. Whether you need information about our services or
+              help with your next transfer, don’t hesitate to reach out. Your
+              satisfaction is our priority.
+            </p>
+            <Row className=" justify-content-center">
+              <Col className="col-5">
+                <Button
+                  variant="gold"
+                  className="custom-gold-btn fw-bold sans p-md-2 mt-3"
+                >
+                  Contact us
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+      <Row className="no-margin-padding justify-content-center mt-4 ">
+        <Col className="no-margin-padding col-8">
+          <h2 className="d-flex justify-content-center text-center ">
+            Frequently Asked Questions
+          </h2>
+        </Col>
+        <Col className="col-12">
+          <Row>
+            <Col >
+           <AccordionFAQ/>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
